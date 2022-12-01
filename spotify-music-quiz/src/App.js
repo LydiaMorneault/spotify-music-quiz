118c1fbdb6c4a326d3ddd51d2c959c93edcce15e
Lydia Morneault <lydia.morneault@gmail.com>
2022-11-30 23:53:20 -0500

    Set up express

--- spotify-music-quiz/src/App.js
+++ spotify-music-quiz/src/App.js
@@ -1,11 +1,32 @@
 import React, { Component } from 'react';
 // import './App.css';
 
+
+
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { apiResponse: ""};
+  }
+
+  callAPI() {
+    fetch("http://localhost:9000/testAPI")
+      .then(res => res.text())
+      .then(res => this.setState({ apiResponse: res }))
+      .catch(err => err);
+  }
+
+  componentDidMount() {
+    this.callAPI();
+  }
+
   render() {
     return (
-      <div>
-        Hello World
+      <div className='App'>
+        <header className='App-header'>
+          <h1>Log In</h1>
+        </header>
+        <p className='App-intro'>{this.state.apiResponse}</p>
       </div>
     );
   }
